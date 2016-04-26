760152d2259891d7e9d5b891a024e13f0c05b3bd
Alvin Tang <atang@udel.edu>
2016-04-25 23:42:03 -0500

    Final

---
+++ PS6_DAL/src/test/java/base/Student_Test.java
@@ -0,0 +1,42 @@
+package base;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import domain.PersonDomainModel;
+import domain.StudentDomainModel;
+
+public class Student_Test{
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddingStudent() {
+	ArrayList<PersonDomainModel>  per = new ArrayList<PersonDomainModel>(); 
+	PersonDomainModel Person1 = new PersonDomainModel();
+   PersonDomainModel addPerson(Person1);}
+  @Test 
+public void testgetStudent
+}
